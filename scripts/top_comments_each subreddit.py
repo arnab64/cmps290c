49b49df2481d993d05fdc9b427f76923888b828a
Arnab Borah <Arnab Borah>
2017-04-30 14:24:17 -0700

    modified file

--- scripts/top_comments_each subreddit.py
+++ scripts/top_comments_each subreddit.py
@@ -1,7 +1,10 @@
 import pandas as pd
-d2 = json.load(codecs.open("2008_01.txt", "r",encoding='utf-8', errors='ignore'))
+import json
+import codecs
+
+d2 = json.load(codecs.open("first_data.json", "r",encoding='utf-8', errors='ignore'))
 for line in d2:
-    print line
+    print(line)
 ff1=pd.DataFrame(d2)    
 ff1 = ff1.drop(['archived','author_flair_css_class','gilded','name','subreddit','body','author_flair_text','controversiality','created_utc','distinguished','edited','retrieved_on','score','score_hidden'], axis=1)
 ff1
